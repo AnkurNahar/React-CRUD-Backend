9f565edd85bc5ec266e3d07af93a2705200d3fbe
Ankur <ankurnahar.an@gmail.com>
2021-02-03 20:52:19 +0600

    cors added

--- loaders/express.js
+++ loaders/express.js
@@ -1,7 +1,9 @@
 const bodyParser = require("body-parser");
 const loadRoutes = require("../api");
+const cors = require('cors');
 
 const expressLoader = (app) => {
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(loadRoutes());
